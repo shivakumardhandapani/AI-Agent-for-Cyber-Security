00e08169ce2068b3e4c66a7ec4e9a48bbb1a91e8
Shiva Kumar Dhandapani <156853108+shivakumardhandapani@users.noreply.github.com>
2025-04-16 20:25:10 -0400

    Update run.py

--- run.py
+++ run.py
@@ -46,7 +46,17 @@ sarsa_config = QAgentConfig(
     save_dir=output_dir + "/results/data/" + str(random_seed)
 )
 
+print("\nEnvironment Information:")
+
 print("Configuration initialized!")
 print(f"Output directory: {output_dir}")
 
-print("\nEnvironment Information:")
+sarsa_agent = SARSAAgent(env, sarsa_config)
+
+print("Starting training...")
+train_result, eval_result = sarsa_agent.train()
+print("Training completed!")
+
+print_summary(train_result, title = "Training")
+print("\n")
+print_summary(eval_result, title = "Evaluation")
